28e06d79bad652487b5d91b897ba3aac018c1703
Charlie BU <butian.charlie@bytedance.com>
2025-11-04 23:49:54 +0800

    routes

--- FE-CAM/src/App.tsx
+++ FE-CAM/src/App.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Navigate to="/api" replace />,
+                element: <Navigate to="/" replace />,
             },
             {
                 path: "api",
--- FE-CAM/src/components/Layout/Sidebar/index.tsx
+++ FE-CAM/src/components/Layout/Sidebar/index.tsx
@@ -1,37 +1,57 @@
-import React from 'react';
-import { Menu } from '@cloud-materials/common';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import styles from './index.module.less';
+import React from "react";
+import { Menu } from "@cloud-materials/common";
+import { IconHome, IconAPIGatewayP } from "@cloud-materials/common/ve-o-iconbox";
+
+import { useNavigate, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import styles from "./index.module.less";
 
 const MenuItem = Menu.Item;
 
 const Sidebar: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { t } = useTranslation();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { t } = useTranslation();
+    const tabList = [
+        {
+            key: "/",
+            icon: <IconHome style={{ width: 20, height: 20 }} />,
+            title: t("nav.home"),
+        },
+        {
+            key: "/api",
+            icon: <IconAPIGatewayP style={{ width: 20, height: 20 }} />,
+            title: t("nav.apiManagement"),
+        },
+    ];
 
-  const handleMenuClick = (key: string) => {
-    navigate(key);
-  };
+    const handleMenuClick = (key: string) => {
+        navigate(key);
+    };
 
-  const getSelectedKeys = () => {
-    const path = location.pathname;
-    if (path.startsWith('/api')) return ['/api'];
-    return ['/api'];
-  };
+    const getSelectedKeys = () => {
+        const path = location.pathname;
+        if (path.startsWith("/api")) return ["/api"];
+        return ["/"];
+    };
 
-  return (
-      <Menu
-        selectedKeys={getSelectedKeys()}
-        onClickMenuItem={handleMenuClick}
-        className={styles.menu}
-      >
-        <MenuItem key="/api">
-          {t('nav.apiManagement')}
-        </MenuItem>
-      </Menu>
-  );
+    return (
+        <Menu
+            selectedKeys={getSelectedKeys()}
+            onClickMenuItem={handleMenuClick}
+            className={styles.menu}
+        >
+            {tabList.map((item) => (
+                <MenuItem
+                    key={item.key}
+                    style={{ display: "flex", alignItems: "center" }}
+                >
+                    {item.icon}
+                    {item.title}
+                </MenuItem>
+            ))}
+        </Menu>
+    );
 };
 
 export default Sidebar;
