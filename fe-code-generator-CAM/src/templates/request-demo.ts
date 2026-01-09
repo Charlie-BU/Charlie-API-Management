f5ff6fb642a21a6ed7ea84df92e2f36f4be132b3
Charlie BU <butian.charlie@bytedance.com>
2026-01-09 21:20:41 +0800

    fix: codegen bug fix

--- fe-code-generator-CAM/src/templates/request-demo.ts
+++ fe-code-generator-CAM/src/templates/request-demo.ts
@@ -1,21 +1,26 @@
-export const requestDemoCode = (demoServiceName: string) => `
+import { capitalizeFirstLetter } from "../utils/utils";
+
+export const requestDemoCode = (demoServiceName: string) => {
+    const demoClassName = capitalizeFirstLetter(`${demoServiceName}Service`);
+    return `
 // This is a demo request code for how to use auto generated service class.
 // You can also use your own request instance like axios or fetch.
 
 import axios, { type AxiosRequestConfig } from "axios"; // Install axios if you haven't
-import ${demoServiceName}Service from "./${demoServiceName}/index";
+import ${demoClassName} from "./${demoServiceName}/index";
 
 const BASE_URL = "http://localhost:3000"; // Change to the actual base URL
 
-export const demoServiceForAxios = new ${demoServiceName}Service<AxiosRequestConfig>({
+export const demoServiceForAxios = new ${demoClassName}<AxiosRequestConfig>({
     baseURL: BASE_URL,
     request: (config, _options) =>
         axios.request({ ...config }).then((res) => res.data), // Pay attention to the serialization of the response data
 });
 
-export const demoServiceForFetch = new ${demoServiceName}Service<RequestInit>({
+export const demoServiceForFetch = new ${demoClassName}<RequestInit>({
     baseURL: BASE_URL,
     request: (config, _options) =>
         fetch(BASE_URL + config.url, { ...config }).then((res) => res.json()), // Pay attention to the serialization of the response data
 });
 `;
+};
